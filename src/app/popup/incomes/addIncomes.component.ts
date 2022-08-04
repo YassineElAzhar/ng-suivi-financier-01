b52ecfaf03a9ea6e05e35d8808e814dd115c3780
YassineElAzhar <yassine.elazhar@gmail.com>
2022-08-03 23:33:23 -0400

    Update addIncomes popup and form

--- src/app/popup/incomes/addIncomes.component.ts
+++ src/app/popup/incomes/addIncomes.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addIncomes',
-  template: `
-    <p>
-      add incomes works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './addIncomes.component.html',
+  styleUrls: ['./addIncomes.component.scss']
 })
 export class AddIncomesComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      typeIncome: [null],
+      provenance: [null, [Validators.required, Validators.minLength(3)]],
+      titre: [null, [Validators.required, Validators.minLength(10)]],
+      dateIncome: [null, [Validators.required]],
+      montant: [null],
+    });
   }
+
+  saveDetails(form: any) {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(form.value, null, 4));
+  }
+
+
+
+
 
 }
